70a480b9c883d3cfe725d3b058f089dcf218a3be
ExequielPina <exequiel_pina@hotmail.es>
2022-12-12 11:54:51 +0100

    commit 1.8 guardando datos en LS y deploy del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Header from "./components/Header"
 import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
 import Paciente from "./components/Paciente";
+
+
 
 
 
@@ -11,6 +13,19 @@ function App() {
   const [pacientes, setPacientes] = useState([]);
 
   const [pacienteEditar, setPacienteEditar] = useState({});
+  
+  useEffect(() => {
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      setPacientes(pacientesLS)
+    }
+    obtenerLS();
+  },[]);
+  
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify( pacientes ));
+  }, [pacientes])
 
   const eliminarPaciente = id => {
       const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id);
